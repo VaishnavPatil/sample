dfba64240b870b03204ef324a88fa9c0fad4c929
Vaishnav Barbade <76620267+VaishnavPatil@users.noreply.github.com>
2024-10-09 20:15:15 +0530

    Update combo-tri.cpp

--- combo-tri.cpp
+++ combo-tri.cpp
@@ -36,3 +36,37 @@ int main()
     }
     return 0;
 }
+
+
+/*#include<bits/stdc++.h>
+using namespace std;
+void solve(int n, int m)
+{
+    for(int i=1; i<=2*n-1; i++)
+    {
+        int stars=i;
+        if(i>n)
+        {
+            stars = 2*n-i;
+        }
+        for(int j=1; j<=stars; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        cin>>n>>m;
+        solve(n, m);
+      
+    }
+    return 0;
+}*/
