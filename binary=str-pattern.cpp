51158e4350a18aeaf114cf9a135193f883f9bff3
Vaishnav Barbade <76620267+VaishnavPatil@users.noreply.github.com>
2024-11-15 12:33:29 +0530

    Update binary=str-pattern.cpp

--- binary=str-pattern.cpp
+++ binary=str-pattern.cpp
@@ -43,5 +43,34 @@ int main()
 }
 
 /*
-  
+  #include<bits/stdc++.h>
+using namespace std;
+void print (int n, int m)
+{
+    int start = 1;
+    for(int i=0; i<n; i++)
+    {
+        if(i%2==0)
+        {
+            start = 1;
+        }
+        else
+        {
+            start = 0;
+        }
+        for(int j=0; j<i; j++)
+        {
+            cout<<start<<" ";
+            start = 1 - start;
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    print(n, m);
+    return 0;
+}
 */
